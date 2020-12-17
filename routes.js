81905adece3258da97856aff3f4604dc14262db6
Clavier Eliott <clavier.eliott.el@gmail.com>
2020-12-17 15:17:29 +0100

    Fix routes

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 // Gestion des routes
-$('li[routeid]').click(function () {
+$('[routeid]').click(function () {
   const id = $(this).attr('routeID');
   $('section').hide();
   $('section[id^="' + id + '"]').show();
